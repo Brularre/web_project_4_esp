ae24faf97dbd72ba4f4fd919f28d180507e9a093
Bruno Larraín <bruno.larrain.e@gmail.com>
2022-08-21 14:35:38 -0400

    refactor: Remplazar la forma en que se arman los elementos cards

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,12 @@
 const pu = document.querySelector(".popup");
 const puCloseBtn = document.querySelector(".popup__close-btn");
-const pfEditBtn = document.querySelector(".profile__edit-btn");
+const editBtn = document.querySelector(".profile__edit-btn");
 const nameInput = document.querySelector("#profile-name");
 const jobInput = document.querySelector("#profile-job");
 
 /* EDIT FORM -> EDIT BUTTON LOGIC */
 
-pfEditBtn.addEventListener("click", function () {
+editBtn.addEventListener("click", function () {
     let username = document.querySelector(".profile__name").textContent;
     let userJob = document.querySelector(".profile__job").textContent;
     pu.classList.add("popup_opened");
@@ -33,3 +33,51 @@ function handleProfileFormSubmit(evt) {
 }
 
 pu.addEventListener("submit", handleProfileFormSubmit);
+
+/* CARD TEMPLATE INJECTOR */
+
+const cards = [
+    {
+        name: "Yellowstone",
+        link: "./images/elements-img-Yellowstone.jpg",
+        alt: "Fotografía del Parque Nacional Yellowstone",
+    },
+    {
+        name: "Empire State",
+        link: "./images/elements-img-EmpireState.jpg",
+        alt: "Ftografía del edificio Empire State en NYC",
+    },
+    {
+        name: "Monte Rushmore",
+        link: "./images/elements-img-Rushmore.jpg",
+        alt: "Fotografía del Monte Rushmore en Dakota del Sur",
+    },
+    {
+        name: "Puerta de las Nubes",
+        link: "./images/elements-img-CloudGate.jpg",
+        alt: "Fotografía de la Cloud Gate en Chicago",
+    },
+    {
+        name: "Golden Gate",
+        link: "./images/elements-img-GoldenGate.jpg",
+        alt: "Fotografía del Puente Golden Gate en San Francisco",
+    },
+    {
+        name: "Times Square",
+        link: "./images/elements-img-TimesSquare.jpg",
+        alt: "Fotografía de Times Square en NYC",
+    },
+];
+
+const cardsContainer = document.querySelector(".elements");
+const templateCards = document.querySelector("#template__cards").content;
+
+for (let key in cards) {
+    const elementsCard = templateCards
+        .querySelector(".elements__card")
+        .cloneNode(true);
+    elementsCard.querySelector(".elements__name").textContent = cards[key].name;
+    elementsCard.querySelector(".elements__image").src = cards[key].link;
+    elementsCard.querySelector(".elements__image").alt = cards[key].alt;
+    cardsContainer.append(elementsCard);
+}
