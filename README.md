511b3d3b78db2d391f2d1b7528f9e6f0b35a46ed
Bruno Larraín <bruno.larrain.e@gmail.com>
2022-09-08 23:41:28 -0400

    Feat: Agrega funcionalidad de cierre de formularios, más mejoras propuestas por tutor

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 <div id="#inicio"></div>
 
-# Alrededor de Estados Unidos - Proyecto Cuarto y Quinto Sprint de Practicum por <a href="https://github.com/brularre/">Bruno Larraín</a>
+# Alrededor de Estados Unidos - Proyecto Cuarto, Quinto y Sexto Sprint de Practicum por <a href="https://github.com/brularre/">Bruno Larraín</a>
 
 ## Sobre el Proyecto
 
-¡Hola! Si estás leyendo esto, estás revisando y/o visitando mi portafolio para ver mi trabajo. Sigue mi progreso en el Curso de Desarrollo Web de Practicum, esta vez estoy en el quinto Sprint, continuando el cuarto que incorpora Javascript y uno de mis puntos favoritos, pues se ve como se abren todas las posibilidades del desarrollo web.
+¡Hola! Si estás leyendo esto, estás revisando y/o visitando mi portafolio para ver mi trabajo. Sigue mi progreso en el Curso de Desarrollo Web de Practicum, este es el cuarto proyecto desarrollado para Practicum, siendo esta la tercera iteración y el sexto sprint del curso. Acá incorporamos validación de formularios personalizada y otros detalles de eventos. 
+
+Este proyecto me frustró bastante pues intenté un enfoque distinto al propuesto, creando y borrando los modales según se requiriera. Me cuesta un poco entender como hacer funcionar algunas funciones, pero en este caso la ayuda del Tutor de Practicum me logró orientar un poco y retomar el camino.
 
 ## Tecnologías usadas
 
@@ -12,10 +14,12 @@
 -   [CSS](https://www.w3.org/TR/CSS/#css)
 -   [Javascript](https://www.javascript.com/)
 
-En este proyecto se profundizo en el uso de lógica Javascript para añadir funcionalidad a la creación de formulario y elementos.
+En este proyecto se incorporó la validación de formularios y otras funcionalidades usando eventos, agregando otras alternativas para cerrar los formularios.
 
 ## Versiones
 
+2.2 - Feat: Agrega funcionalidad de cierre de formularios, más unas mejoras propuestas por Tutor Practicum.
+2.1 - Feat: Agrega validación básica de formularios.
 2.0 - Feat: Commit con los nuevos cambios para entrega y revisión.
 1.7 - Fix: Segunda entrega con correcciones para revisor.
 1.6 - Fix: Entrega con correcciones para revisor.
--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,7 @@ const addBtn = document.querySelector(".profile__add-btn");
 const editBtn = document.querySelector(".profile__edit-btn");
 const profileName = document.querySelector(".profile__name");
 const profileJob = document.querySelector(".profile__job");
+const popupCheckingTimeout = 300;
 
 /* FUNCTIONS */
 
@@ -101,130 +102,127 @@ const closeOnEsc = (evt) => {
 };
 
 const closeOnOutsideClick = (evt) => {
-  closeElement(evt);
-  // const closedElement = evt.currentTarget;
-  // closeElement(closedElement);
-  // closedElement.removeEventListener("click", closeOnOutsideClick);
+  const parent = evt.target.closest(".popup");
+  if (!parent) {
+    closeElement(evt);
+  }
 };
-// document.removeEventListener("click", closeOnOutsideClick);
-//   if (modalBox && evt.target === modalBox) {
-//     console.log("a");
-//     modalBox.remove();
-//     window.removeEventListener("click", closeOnOutsideClick);
-//   }
-// };
-// document.addEventListener("click", (e) => {
-//   // Check if the filter list parent element exist
-//   const isClosest = e.target.closest(popupQuerySelector);
-
-//   // If `isClosest` equals falsy & popup has the class `show`
-//   // then hide the popup
-//   if (!isClosest && popupEl.classList.contains("show")) {
-//     popupEl.classList.remove("show");
-//   }
-// });
 
 /* ADD FORM CREATOR */
 
 addBtn.addEventListener("click", () => {
-  const newPopup = createPopup();
-  const popup = newPopup.querySelector(".popup");
-  const popupClose = popup.querySelector(".popup__close-btn");
-  const [placeName, placeLink] = setPopupVariables(popup);
-  configInputs(
-    popup,
-    "setFirstInput",
-    "Nuevo lugar",
-    "place-name",
-    "place-name",
-    "Título",
-    "text",
-    "2",
-    "30",
-    "popup__error-place-name"
-  );
-  configInputs(
-    popup,
-    "setSecondInput",
-    "",
-    "place-link",
-    "place-link",
-    "Enlace a la imagen",
-    "url",
-    "",
-    "",
-    "popup__error-place-link"
-  );
-  popupClose.addEventListener("click", (evt) => closeElement(evt));
-  popup.addEventListener("submit", (evt) => {
-    evt.preventDefault();
-    addNewCard(placeName.value, placeLink.value);
-    closeElement(evt);
-  });
-  enableValidation(popup, {
-    formSelector: ".popup",
-    inputSelector: ".popup__input",
-    submitButtonSelector: ".popup__submit-btn",
-    inactiveButtonClass: ".popup__submit-btn_inactive",
-    inputErrorClass: ".popup__input_error",
-    errorClass: ".popup__error-msg",
-  });
-  document.body.prepend(popup);
-  document.addEventListener("keydown", closeOnEsc);
-  popup.addEventListener("click", closeOnOutsideClick);
+  const activePopup = document.querySelector(".popup");
+  if (activePopup) {
+    return;
+  }
+  setTimeout(() => {
+    const newPopup = createPopup();
+    const popup = newPopup.querySelector(".popup");
+    const popupClose = popup.querySelector(".popup__close-btn");
+    const [placeName, placeLink] = setPopupVariables(popup);
+    configInputs(
+      popup,
+      "setFirstInput",
+      "Nuevo lugar",
+      "place-name",
+      "place-name",
+      "Título",
+      "text",
+      "2",
+      "30",
+      "popup__error-place-name"
+    );
+    configInputs(
+      popup,
+      "setSecondInput",
+      "",
+      "place-link",
+      "place-link",
+      "Enlace a la imagen",
+      "url",
+      "",
+      "",
+      "popup__error-place-link"
+    );
+    popupClose.addEventListener("click", (evt) => closeElement(evt));
+    popup.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      addNewCard(placeName.value, placeLink.value);
+      closeElement(evt);
+    });
+    enableValidation(popup, {
+      formSelector: ".popup",
+      inputSelector: ".popup__input",
+      submitButtonSelector: ".popup__submit-btn",
+      inactiveButtonClass: ".popup__submit-btn_inactive",
+      inputErrorClass: ".popup__input_error",
+      errorClass: ".popup__error-msg",
+    });
+    document.body.prepend(popup);
+    document.addEventListener("keydown", closeOnEsc);
+    popup.addEventListener("click", closeOnOutsideClick);
+  }, popupCheckingTimeout);
 });
 
 /* EDIT FORM CREATOR */
 
 editBtn.addEventListener("click", () => {
-  const newPopup = createPopup();
-  const popup = newPopup.querySelector(".popup");
-  const popupClose = popup.querySelector(".popup__close-btn");
-  const [nameInput, jobInput] = setPopupVariables(popup);
-  configInputs(
-    popup,
-    "setFirstInput",
-    "Editar perfil",
-    "profile-name",
-    "profile-name",
-    "Usuario",
-    "text",
-    "2",
-    "40",
-    "popup__error-profile-name"
-  );
-  configInputs(
-    popup,
-    "setSecondInput",
-    "",
-    "profile-job",
-    "profile-job",
-    "Ocupación",
-    "text",
-    "2",
-    "200",
-    "popup__error-profile-job"
-  );
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileJob.textContent;
-  popupClose.addEventListener("click", (evt) => closeElement(evt));
-  popup.addEventListener("submit", (evt) => {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    closeElement(evt);
-  });
-  enableValidation(popup, {
-    formSelector: ".popup",
-    inputSelector: ".popup__input",
-    submitButtonSelector: ".popup__submit-btn",
-    inactiveButtonClass: ".popup__submit-btn_inactive",
-    inputErrorClass: ".popup__input_error",
-    errorClass: ".popup__error-msg",
-  });
-  document.body.prepend(popup);
-  document.addEventListener("keydown", closeOnEsc);
-  popup.addEventListener("focusout", closeOnOutsideClick);
+  const activePopup = document.querySelector(".popup");
+  if (activePopup) {
+    return;
+  }
+  setTimeout(() => {
+    const newPopup = createPopup();
+    const popup = newPopup.querySelector(".popup");
+    const popupClose = popup.querySelector(".popup__close-btn");
+    const [nameInput, jobInput] = setPopupVariables(popup);
+    configInputs(
+      popup,
+      "setFirstInput",
+      "Editar perfil",
+      "profile-name",
+      "profile-name",
+      "Usuario",
+      "text",
+      "2",
+      "40",
+      "popup__error-profile-name"
+    );
+    configInputs(
+      popup,
+      "setSecondInput",
+      "",
+      "profile-job",
+      "profile-job",
+      "Ocupación",
+      "text",
+      "2",
+      "200",
+      "popup__error-profile-job"
+    );
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+    popupClose.addEventListener("click", (evt) => closeElement(evt));
+    popup.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      profileName.textContent = nameInput.value;
+      profileJob.textContent = jobInput.value;
+      closeElement(evt);
+    });
+    enableValidation(popup, {
+      formSelector: ".popup",
+      inputSelector: ".popup__input",
+      submitButtonSelector: ".popup__submit-btn",
+      inactiveButtonClass: ".popup__submit-btn_inactive",
+      inputErrorClass: ".popup__input_error",
+      errorClass: ".popup__error-msg",
+    });
+    document.body.prepend(popup);
+    document.addEventListener("keydown", closeOnEsc);
+    document.addEventListener("click", (event) => {
+      closeOnOutsideClick(event);
+    });
+  }, popupCheckingTimeout);
 });
 
 /* CARD TEMPLATE INJECTOR */
@@ -300,17 +298,25 @@ function addNewCard(placeName, placeLink) {
 /* MODAL BOX LOGIC */
 
 function openModalImage(img) {
-  const templateModal = document.querySelector("#template_modal-box").content;
-  const modalBox = templateModal.cloneNode(true);
-  modalBox.id = "clonedModalBox";
-  const modalImg = modalBox.querySelector(".modal-box__image");
-  const modalTitle = modalBox.querySelector(".modal-box__title");
-  const modalCloseBtn = modalBox.querySelector(".modal-box__close-btn");
-  modalImg.src = img.src;
-  modalImg.alt = img.alt;
-  modalTitle.textContent = img.closest("div").textContent;
-  modalCloseBtn.addEventListener("click", (evt) => closeElement(evt));
-  document.body.prepend(modalBox);
-  document.addEventListener("keydown", closeOnEsc);
-  modalBox.addEventListener("focusout", (evt) => closeOnOutsideClick(evt));
+  const activePopup = document.querySelector(".popup");
+  if (activePopup) {
+    return;
+  }
+  setTimeout(() => {
+    const templateModal = document.querySelector("#template_modal-box").content;
+    const modalBox = templateModal.cloneNode(true);
+    modalBox.id = "clonedModalBox";
+    const modalImg = modalBox.querySelector(".modal-box__image");
+    const modalTitle = modalBox.querySelector(".modal-box__title");
+    const modalCloseBtn = modalBox.querySelector(".modal-box__close-btn");
+    modalImg.src = img.src;
+    modalImg.alt = img.alt;
+    modalTitle.textContent = img.closest("div").textContent;
+    modalCloseBtn.addEventListener("click", (evt) => closeElement(evt));
+    document.body.prepend(modalBox);
+    document.addEventListener("keydown", closeOnEsc);
+    document.addEventListener("click", (event) => {
+      closeOnOutsideClick(event);
+    });
+  }, popupCheckingTimeout);
 }
